12a3eeb179daf16da2e9c28e0ba5604c079bdb5a
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2020-03-30 15:13:56 -0300

    fix

--- generator.js
+++ generator.js
@@ -88,247 +88,225 @@ async function execute(config) {
         //content += `    private api = '${c.toLowerCase()}';\n`;
         content += `    constructor(${connectorDecorator}private connector: OpenApiConnector) {}\n`;
         for (let a in client.actions) {
-            let fileName = `${changeCase.paramCase(c)}.client.ts`;
-            let client = clients[c];
+            let action = client.actions[a];
+            let path = action.path;
+            let returnType = 'void';
 
-            let context = new ResolutionContext('client');
+            let bodyRef = 'undefined';
+            let parametersRef = 'undefined';
 
-            let content = '';
+            let bodySignature = '';
+            let parametersSignature = '';
 
-            content += `import { OpenApiConnector } from '@cblx-br/openapi-typescript';\n`;
-
-            if (events.beforeWriteServiceClass) {
-                content += events.beforeWriteServiceClass();
+            function getParamName(name) {
+                return name.startsWith('$') ? name : changeCase.camelCase(name);
             }
 
-            let connectorDecorator = '';
-            if (events.createConnectorDecorator) {
-                connectorDecorator = events.createConnectorDecorator();
+            //let parametersRef = 'undefined';
+            if ('parameters' in action) {
+                //parametersRef = 'parameters';
+                parametersSignature += 'parameters: { ';
+                let parameters = [];
+                parameters = action.parameters;
+                parametersSignature += parameters.map(p => getParamName(p.name) + '?: ' + context.resolve(p.schema)).join(', ');
+                parametersSignature += ' }';
+
+                let queryParameters = parameters.filter(p => p.in == 'query');
+                if (queryParameters.length) {
+                    parametersRef = '{ ';
+                    parametersRef += queryParameters.map(p => `${getParamName(p.name)}: parameters.${getParamName(p.name)}`).join(', ');
+                    parametersRef += ' }';
+                }
+
+                let pathParameters = parameters.filter(p => p.in == 'path');
+                for(let p of pathParameters){
+                    path = path.replace(`{${p.name}}`, `$\{parameters.${p.name}\}`);
+                }
             }
 
-            content += `export class ${c}Client {\n`;
-            //content += `    private api = '${c.toLowerCase()}';\n`;
-            content += `    constructor(${connectorDecorator}private connector: OpenApiConnector) {}\n`;
-            for (let a in client.actions) {
-                let action = client.actions[a];
-                let path = action.path;
-                let returnType = 'void';
+            if ('requestBody' in action) {
+                let content = action.requestBody.content['application/json'];
+                if (!content) { continue; }
 
-                let bodyRef = 'undefined';
-                let parametersRef = 'undefined';
-
-                let bodySignature = '';
-                let parametersSignature = '';
-
-                function getParamName(name) {
-                    return name.startsWith('$') ? name : changeCase.camelCase(name);
-                }
-
-                //let parametersRef = 'undefined';
-                if ('parameters' in action) {
-                    //parametersRef = 'parameters';
-                    parametersSignature += 'parameters: { ';
-                    let parameters = [];
-                    parameters = action.parameters;
-                    parametersSignature += parameters.map(p => getParamName(p.name) + '?: ' + context.resolve(p.schema)).join(', ');
-                    parametersSignature += ' }';
-
-                    let queryParameters = parameters.filter(p => p.in == 'query');
-                    if (queryParameters.length) {
-                        parametersRef = '{ ';
-                        parametersRef += queryParameters.map(p => `${getParamName(p.name)}: parameters.${getParamName(p.name)}`).join(', ');
-                        parametersRef += ' }';
-                    }
-
-                    let pathParameters = parameters.filter(p => p.in == 'path');
-                    for (let p of pathParameters) {
-                        path = path.replace(`{${p.name}}`, `$\{parameters.${p.name}\}`);
-                    }
-                }
-
-                if ('requestBody' in action) {
-                    let content = action.requestBody.content['application/json'];
-                    if (!content) { continue; }
-
-                    bodyRef = 'body';
-                    let modelType = context.resolve(action.requestBody.content['application/json'].schema);
-                    bodySignature += `body: ${modelType}`;
-                }
-
-                if ('responses' in action && '200' in action.responses && 'content' in action.responses['200'] && 'application/json' in action.responses['200'].content) {
-                    let schema = action.responses['200'].content['application/json'].schema;
-                    returnType = context.resolve(schema);
-                }
-
-                let signatureParts = [];
-                if (bodySignature) { signatureParts.push(bodySignature); }
-                if (parametersSignature) { signatureParts.push(parametersSignature); }
-                let signature = signatureParts.join(', ');
-
-
-                content += `    ${a} = (${signature}) : Promise<${returnType}> => {\n`;
-
-
-                content += `        return this.connector.request(\n`;
-                content += `            '${action.httpMethod}',\n`;
-                content += `            \`${path}\`,\n`;
-                content += `            ${parametersRef},\n`;
-                content += `            ${bodyRef}\n`;
-                content += `        );\n`;
-
-                // content += `        return this.invoker.invoke(\n`;
-                // //Esse primeiros parametros eram usados pelo webtyped para tipar o subscription de eventos. Vamos simplificar mandando qlquer coisa
-                // content += `            {\n`;
-                // content += `                returnTypeName: 'notNecessary',\n`;
-                // content += `                kind: 'notNecessary',\n`;
-                // content += `                func: <any>this.${a},\n`;
-                // content += `                parameters: {},\n`;
-                // content += `            },\n`;
-                // //Caminho base da api (não precisa, vai estar tudo no path da action)
-                // content += `            '',\n`;
-                // //Caminho relativo da ação
-                // content += `            '${action.path}',\n`;
-                // //Metodo http utilizado
-                // content += `            '${action.httpMethod}',\n`;
-                // //Body
-                // content += `            ${bodyRef},\n`;
-                // //Query parameters
-                // content += `            ${parametersRef},\n`;
-                // //Retorno esperado, neste projeto usamos direto Promise,  no lugar de Observables
-                // content += `            { name: 'Promise' }\n`;
-                // content += `        );\n`;
-
-                content += `    }\n`;
-            }
-            content += `}\n`;
-
-            for (let i in context.imports) {
-                content = i + content;
+                bodyRef = 'body';
+                let modelType = context.resolve(action.requestBody.content['application/json'].schema);
+                bodySignature += `body: ${modelType}`;
             }
 
-            let filePath = path.join(targetDir, fileName);
-            fs.writeFileSync(filePath, content);
-            allFiles.push(filePath);
+            if ('responses' in action && '200' in action.responses && 'content' in action.responses['200'] && 'application/json' in action.responses['200'].content) {
+                let schema = action.responses['200'].content['application/json'].schema;
+                returnType = context.resolve(schema);
+            }
+
+            let signatureParts = [];
+            if (bodySignature) { signatureParts.push(bodySignature); }
+            if (parametersSignature) { signatureParts.push(parametersSignature); }
+            let signature = signatureParts.join(', ');
+
+
+            content += `    ${a} = (${signature}) : Promise<${returnType}> => {\n`;
+
+
+            content += `        return this.connector.request(\n`;
+            content += `            '${action.httpMethod}',\n`;
+            content += `            \`${path}\`,\n`;
+            content += `            ${parametersRef},\n`;
+            content += `            ${bodyRef}\n`;
+            content += `        );\n`;
+
+            // content += `        return this.invoker.invoke(\n`;
+            // //Esse primeiros parametros eram usados pelo webtyped para tipar o subscription de eventos. Vamos simplificar mandando qlquer coisa
+            // content += `            {\n`;
+            // content += `                returnTypeName: 'notNecessary',\n`;
+            // content += `                kind: 'notNecessary',\n`;
+            // content += `                func: <any>this.${a},\n`;
+            // content += `                parameters: {},\n`;
+            // content += `            },\n`;
+            // //Caminho base da api (não precisa, vai estar tudo no path da action)
+            // content += `            '',\n`;
+            // //Caminho relativo da ação
+            // content += `            '${action.path}',\n`;
+            // //Metodo http utilizado
+            // content += `            '${action.httpMethod}',\n`;
+            // //Body
+            // content += `            ${bodyRef},\n`;
+            // //Query parameters
+            // content += `            ${parametersRef},\n`;
+            // //Retorno esperado, neste projeto usamos direto Promise,  no lugar de Observables
+            // content += `            { name: 'Promise' }\n`;
+            // content += `        );\n`;
+
+            content += `    }\n`;
+        }
+        content += `}\n`;
+
+        for (let i in context.imports) {
+            content = i + content;
         }
 
-
-        //Criar as models
-        let modelDir = path.join(targetDir, 'models');
-        if (!fs.existsSync(modelDir)) {
-            fs.mkdirSync(modelDir);
-        }
-
-        for (let c in json.components.schemas) {
-            //Alguns tipos array virão com [], quando dentro de generics
-            let typeName = c.replace('[]', 'Array');
-
-            //let imports = {};
-            let context = new ResolutionContext('model', typeName);
-            let fileName = `${changeCase.paramCase(typeName)}.ts`;
-            let content = '';
-            let type = json.components.schemas[c];
-
-            if ('enum' in type) {
-                content += `export enum ${typeName} {\n`;
-                content += `  ${type.enum.map(e => `_${e} = ${e}`).join(',\n  ')}\n`;
-                content += '}';
-            } else {
-                //Crio a interface
-                content += `export interface ${typeName} {\n`;
-                for (let p in type.properties) {
-                    let prop = type.properties[p];
-                    let propTypeName = context.resolve(prop);
-                    content += `    '${p}'${prop.nullable ? '?' : ''}: ${propTypeName};\n`;
-                }
-                content += '}\n\n';
-
-                content += `const schema = ${JSON.stringify(type, null, 4)};\n\n`
-
-                //Crio um tipo fisico para referenciarmos names
-                content += `export class ${typeName} {\n`;
-                content += `    static '$' = '${typeName}';\n`;
-
-                //Importante manter o $ aqui, uma vez que nomes de campos
-                //podem conflitar com coisas nativas do javascript.
-                //Por exemplo, o campo 'name' conflitaria com Function.name,
-                //que está em toda classe.
-                for (let p in type.properties) {
-                    content += `    static '$${p}' = '${p}';\n`;
-                }
-                content += `    static schema = schema;\n`;
-                content += `}`;
-            }
-
-
-            for (let i in context.imports) {
-                content = i + content;
-            }
-
-            let filePath = path.join(modelDir, fileName);
-            fs.writeFileSync(filePath, content);
-            allFiles.push(filePath);
-        }
-
-        let indexContent = '';
-        for (let filePath of allFiles) {
-            indexContent += `export * from '${filePath.replace('.ts', '').replace(/\\/g, '/')}';\n`;
-        }
-        fs.writeFileSync(path.join(targetDir, 'index.ts'), indexContent);
+        let filePath = path.join(targetDir, fileName);
+        fs.writeFileSync(filePath, content);
+        allFiles.push(filePath);
     }
 
-    class ResolutionContext {
-        constructor(scope, typeName) {
-            //this.path = path;
-            this.imports = {};
-            this.importsPath = '';
-            this.typeName = typeName;
-            if (scope == 'client') {
-                this.importsPath = '/models';
-            }
-        }
 
-        resolve(schema) {
-            if (schema.allOf) {
-                schema.$ref = schema.allOf[0].$ref;
-            }
-
-            if (schema.$ref) {
-                let typeName = schema.$ref.split('/').reverse()[0];
-
-                // Avoid self referencing importing
-                if (this.typeName != typeName) {
-
-                    //Alguns tipos array virão com [], quando dentro de generics
-                    typeName = typeName.replace('[]', 'Array');
-
-                    this.imports[`import { ${typeName} } from '.${this.importsPath}/${changeCase.paramCase(typeName)}';\n`] = true;
-                }
-                return typeName;
-            }
-
-            switch (schema.type) {
-                case 'array': return `Array<${this.resolve(schema.items)}>`;
-                case 'boolean': return 'boolean';
-                case 'integer': return 'number';
-                case 'number': return 'number';
-                case 'string': return 'string';
-            }
-            return 'any';
-        }
-
+    //Criar as models
+    let modelDir = path.join(targetDir, 'models');
+    if (!fs.existsSync(modelDir)) {
+        fs.mkdirSync(modelDir);
     }
 
-    const deleteFolderRecursive = function (dir) {
-        if (fs.existsSync(dir)) {
-            fs.readdirSync(dir).forEach((file, index) => {
-                const curPath = path.join(dir, file);
-                if (fs.lstatSync(curPath).isDirectory()) { // recurse
-                    deleteFolderRecursive(curPath);
-                } else { // delete file
-                    fs.unlinkSync(curPath);
-                }
-            });
-        }
-    };
+    for (let c in json.components.schemas) {
+        //Alguns tipos array virão com [], quando dentro de generics
+        let typeName = c.replace('[]', 'Array');
 
-    module.exports = execute;
+        //let imports = {};
+        let context = new ResolutionContext('model', typeName);
+        let fileName = `${changeCase.paramCase(typeName)}.ts`;
+        let content = '';
+        let type = json.components.schemas[c];
+
+        if ('enum' in type) {
+            content += `export enum ${typeName} {\n`;
+            content += `  ${type.enum.map(e => `_${e} = ${e}`).join(',\n  ')}\n`;
+            content += '}';
+        } else {
+            //Crio a interface
+            content += `export interface ${typeName} {\n`;
+            for (let p in type.properties) {
+                let prop = type.properties[p];
+                let propTypeName = context.resolve(prop);
+                content += `    '${p}'${prop.nullable ? '?' : ''}: ${propTypeName};\n`;
+            }
+            content += '}\n\n';
+
+            content += `const schema = ${JSON.stringify(type, null, 4)};\n\n`
+
+            //Crio um tipo fisico para referenciarmos names
+            content += `export class ${typeName} {\n`;
+            content += `    static '$' = '${typeName}';\n`;
+
+            //Importante manter o $ aqui, uma vez que nomes de campos
+            //podem conflitar com coisas nativas do javascript.
+            //Por exemplo, o campo 'name' conflitaria com Function.name,
+            //que está em toda classe.
+            for (let p in type.properties) {
+                content += `    static '$${p}' = '${p}';\n`;
+            }
+            content += `    static schema = schema;\n`;
+            content += `}`;
+        }
+
+
+        for (let i in context.imports) {
+            content = i + content;
+        }
+
+        let filePath = path.join(modelDir, fileName);
+        fs.writeFileSync(filePath, content);
+        allFiles.push(filePath);
+    }
+
+    let indexContent = '';
+    for (let filePath of allFiles) {
+        indexContent += `export * from '${filePath.replace('.ts', '').replace(/\\/g, '/')}';\n`;
+    }
+    fs.writeFileSync(path.join(targetDir, 'index.ts'), indexContent);
+}
+
+class ResolutionContext {
+    constructor(scope, typeName) {
+        //this.path = path;
+        this.imports = {};
+        this.importsPath = '';
+        this.typeName = typeName;
+        if (scope == 'client') {
+            this.importsPath = '/models';
+        }
+    }
+
+    resolve(schema) {
+        if (schema.allOf) {
+            schema.$ref = schema.allOf[0].$ref;
+        }
+
+        if (schema.$ref) {
+            let typeName = schema.$ref.split('/').reverse()[0];
+
+            // Avoid self referencing importing
+            if (this.typeName != typeName) {
+
+                //Alguns tipos array virão com [], quando dentro de generics
+                typeName = typeName.replace('[]', 'Array');
+
+                this.imports[`import { ${typeName} } from '.${this.importsPath}/${changeCase.paramCase(typeName)}';\n`] = true;
+            }
+            return typeName;
+        }
+
+        switch (schema.type) {
+            case 'array': return `Array<${this.resolve(schema.items)}>`;
+            case 'boolean': return 'boolean';
+            case 'integer': return 'number';
+            case 'number': return 'number';
+            case 'string': return 'string';
+        }
+        return 'any';
+    }
+
+}
+
+const deleteFolderRecursive = function (dir) {
+    if (fs.existsSync(dir)) {
+        fs.readdirSync(dir).forEach((file, index) => {
+            const curPath = path.join(dir, file);
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+    }
+};
+
+module.exports = execute;
