54895f5fa4ffe8acf6a766be55e22814fad62ddd
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2020-10-20 17:46:19 -0300

    supporting string enums

--- tool/enum-type.ts
+++ tool/enum-type.ts
@@ -11,8 +11,15 @@ export class EnumType extends TypeBase {
         let schema = this.schema;
         content += `export enum ${this.name} {\n`;
         const names = schema['x-enum-varnames'] || schema.enum.map(e => `_${e}`);
-        content += `  ${schema.enum.map((e, i) => `${names[i]} = ${e}`).join(',\n  ')}\n`;
+        content += `  ${schema.enum.map((e, i) => `${names[i]} = ${this.writeEnumValue(e)}`).join(',\n  ')}\n`;
         content += '}';
         return content;
+    }
+
+    private writeEnumValue(value: any){
+        if(typeof value === 'number'){
+            return value;
+        }
+        return `'${value}'`;
     }
 }
