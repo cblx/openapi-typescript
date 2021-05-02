3136537d11db4c89d5d339617079e3669596b32b
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2021-05-02 16:35:44 -0300

    (breaking change - v0.18.0) new function parameters structure with { path, query, body }

--- tool/client-method.ts
+++ tool/client-method.ts
@@ -12,18 +12,18 @@ export class ClientMethod {
     private path: string;
 
     // To know if we need to import 'deleteUndefineds'
-    get hasQueryParameters(){
+    get hasQueryParameters() {
         return this.queryAndPathParameters && this.queryAndPathParameters.some(x => x.in == 'query');
     }
 
-    get hasUrlParameters(){
+    get hasUrlParameters() {
         return this.queryAndPathParameters && this.queryAndPathParameters.length > 0;
     }
 
-    get hasBodyParameters(){
+    get hasBodyParameters() {
         return this.bodyParameter != null;
     }
-    
+
     constructor(id: string, private pathItem: PathItemObject) {
         this.name = id;
 
@@ -35,13 +35,13 @@ export class ClientMethod {
             let queryParameters = <ParameterObject[]>parameters.filter((p: ParameterObject) => p.in == 'query');
             if (queryParameters.length) {
                 searchPart = 'let search = deleteUndefineds({ ';
-                searchPart += queryParameters.map(p => `${this.getParamName(p.name)}: parameters.${this.getParamName(p.name)}`).join(', ');
+                searchPart += queryParameters.map(p => `${this.getParamName(p.name)}: parameters.query.${this.getParamName(p.name)}`).join(', ');
                 searchPart += ' })';
             }
 
             let pathParameters = <ParameterObject[]>parameters.filter((p: ParameterObject) => p.in == 'path');
             for (let p of pathParameters) {
-                this.path = this.path.replace(`{${p.name}}`, `$\{parameters.${this.getParamName(p.name)}\}`);
+                this.path = this.path.replace(`{${p.name}}`, `$\{parameters.path.${this.getParamName(p.name)}\}`);
             }
         }
         this.preBodyLines.push(searchPart);
@@ -52,9 +52,6 @@ export class ClientMethod {
             if (requestBodyContent) {
                 this.bodyParameter = requestBodyContent.schema;
             }
-        }
-        if (this.bodyParameter == null) {
-            this.preBodyLines.push('let body = undefined;');
         }
 
         if ('responses' in pathItem && '200' in pathItem.responses && 'content' in pathItem.responses['200'] && 'application/json' in pathItem.responses['200'].content) {
@@ -69,30 +66,39 @@ export class ClientMethod {
         return name.startsWith('$') ? name : changeCase.camelCase(name);
     }
 
-    private writeUrlParametersPart(context: TypeContext) {
-        let sgn = 'parameters: { ';
-        let parameters = this.queryAndPathParameters;
+
+    writeParameters(context: TypeContext, spacing: string = '') {
+        const pathParameters = this.queryAndPathParameters?.filter(p => p.in == 'path') || [];
+        const queryParameters = this.queryAndPathParameters?.filter(p => p.in == 'query') || [];
+        if (!queryParameters.length && !pathParameters.length && !this.bodyParameter) { return ''; }
+
+        let parametersText = 'parameters: {\n';
+
+        let parts: string[] = [];
+        if (pathParameters.length) {
+            parts.push(`${spacing}${spacing}path: ${this.writeUrlParameters(context, pathParameters)}`);
+        }
+        if (queryParameters.length) {
+            parts.push(`${spacing}${spacing}query: ${this.writeUrlParameters(context, queryParameters)}`);
+        }
+        if (this.bodyParameter) {
+            parts.push(`${spacing}${spacing}body: ${context.writeName(this.bodyParameter)}`);
+        }
+
+        parametersText += parts.join(',\n');
+        parametersText += `\n${spacing}}`;
+
+        return parametersText;
+    }
+
+    private writeUrlParameters(context: TypeContext, parameters: ParameterObject[]) {
+        let sgn = '{ ';
         sgn += parameters.map((p: ParameterObject) => this.getParamName(p.name) + (p.required ? '' : '?') + ': ' + context.writeName(p.schema)).join(', ');
         sgn += ' }';
         return sgn;
     }
 
-    writeParameters(context: TypeContext){
-        let signatureParts = [];
-        if(this.queryAndPathParameters && this.queryAndPathParameters.length){
-            signatureParts.push(this.writeUrlParametersPart(context));
-        }
-
-        if(this.bodyParameter){
-            signatureParts.push(`body: ${context.writeName(this.bodyParameter)}`);
-        }
-
-        let signature = signatureParts.join(', ');
-
-        return signature;
-    }
-
-    writeReturnType(context: TypeContext){
+    writeReturnType(context: TypeContext) {
         return `Promise<${context.writeName(this.returnType)}>`;
     }
 
@@ -102,7 +108,11 @@ export class ClientMethod {
         bodyRequest += `    '${this.pathItem.httpMethod}',\n`;
         bodyRequest += `    \`${this.path}\`,\n`;
         bodyRequest += `    search,\n`;
-        bodyRequest += `    body\n`;
+        if (this.bodyParameter) {
+            bodyRequest += `    parameters.body\n`;
+        }else{
+            bodyRequest += `    undefined\n`;
+        }
         bodyRequest += `);\n`;
 
         let bodyLines = [...this.preBodyLines];
@@ -111,10 +121,10 @@ export class ClientMethod {
         bodyLines.push('return result;');
 
         const writingClientMethod = config?.hooks?.writingClientMethod;
-        writingClientMethod && writingClientMethod(this,bodyLines,context);
+        writingClientMethod && writingClientMethod(this, bodyLines, context);
 
         let content = '';
-        let parameters = this.writeParameters(context);
+        let parameters = this.writeParameters(context, spacing);
         let returnType = this.writeReturnType(context);
 
         content += `${spacing}async ${this.name}(${parameters}) : ${returnType} {\n`;
